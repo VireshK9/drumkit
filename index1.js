8d4a8e5752cf825dd4670a37efaafe95b0b173f0
VireshK9 <106135983+VireshK9@users.noreply.github.com>
2022-12-06 08:57:03 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -6,17 +6,17 @@ for (var i= 0; i<buttonLength ; i++ ){
         
         switch (buttonInnerHTML) {
             case "w":
-                var tom1 = new Audio ("sounds/tom-1.mp3");
+                var tom1 = new Audio ("tom-1.mp3");
                 tom1.play();
             break;
             
             case "a":
-                var tom2 = new Audio ("sounds/tom-2.mp3");
+                var tom2 = new Audio ("tom-2.mp3");
                 tom2.play();
             break;
             
             case "s":
-                var tom3 = new Audio ("sounds/tom-3.mp3");
+                var tom3 = new Audio ("tom-3.mp3");
                 tom3.play();
             break;
 
